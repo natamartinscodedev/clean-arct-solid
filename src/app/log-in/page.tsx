f9cca94c239bf50982ba0b9b9314386a5384d9c7
natamartinscodedev <nata.codedev@gmail.com>
2025-11-07 11:44:33 -0300

    feat: refactore page login

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -2,8 +2,10 @@
 
 import { authenticateUser } from "@/services/auth";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
+  const router = useRouter();
   // Essa pagina de login, fica responsavel por capturar os dados do usuario
   // e chamar a funcao de autenticação do serviço auth.ts, apenas isso!
   // caso mude futuramente de framework ou biblioteca, só alteramos a pagina de login
@@ -12,52 +14,60 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState(false);
+  const [messageError, setMessageError] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     // função para capturar o submit do form de login
     event.preventDefault();
     setLogin(true);
-    const { userId, token } = await authenticateUser({ email, password });
-    console.log("Usuário logado com ID:", userId, "Token:", token);
 
-    if (token) {
-      // criar tratamento de menssagem de sucesso ou erro em aquivo separado
+    const { userId, status } = await authenticateUser({
+      email,
+      password,
+    });
+
+    if (status !== 200) {
       setLogin(false);
-      // redirecionar para a pagina protegida ou dashboard
-      console.log("Redirecionando para a página protegida...");
+      setMessageError("Falha no login. Verifique suas credenciais.");
     } else {
       setLogin(false);
-      console.log("Falha no login. Verifique suas credenciais.");
+      // redirecionar para a pagina protegida ou dashboard
+      router.push(`/dashboard/${userId}`);
     }
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} method="post" className="form_login">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" disabled={login === true}>
-          Login
-        </button>
-      </form>
+      <div className="container_login">
+        <form onSubmit={handleSubmit} method="post" className="form_login">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" disabled={login === true}>
+            Login
+          </button>
+          {messageError && (
+            <p className="message_login-error">{messageError}</p>
+          )}
+        </form>
+      </div>
     </>
   );
 };
